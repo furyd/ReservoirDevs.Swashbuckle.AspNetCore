927f0fe76050aa9da5d27564135112ff363f9798
dave fury <david.fury@reservoir-devs.com>
2021-08-20 17:06:44 +0100

    feat: added in configuration settings class

---
+++ src/Swashbuckle.AspNetCore.Cli/Settings/ConfigurationSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace Swashbuckle.AspNetCore.Cli.Settings
+{
+    public class ConfigurationSettings
+    {
+        public bool OutputJson { get; set; }
+
+        public bool OutputYaml { get; set; }
+
+        public bool SerializeAsV2 { get; set; }
+
+        public string Assembly { get; set; }
+
+        public string BasePath { get; set; }
+
+        public string Host { get; set; }
+
+        public string Output { get; set; }
+
+        public string SwaggerDoc { get; set; }
+
+        public bool HasHost => !string.IsNullOrWhiteSpace(Host);
+
+        public bool HasBasePath => !string.IsNullOrWhiteSpace(BasePath);
+
+        public bool LoopThroughVersions => string.IsNullOrWhiteSpace(SwaggerDoc);
+
+        public bool HasOutput => !string.IsNullOrWhiteSpace(Output);
+
+        public string DepsFile => Assembly.Replace(".dll", ".deps.json");
+
+        public string RuntimeConfig => Assembly.Replace(".dll", ".runtimeconfig.json");
+    }
+}
